3c2a77cc27c2fe90eab698ca271472453be2a8a5
ebussiere <eddy@embtech.ca>
2020-09-25 01:27:31 +0000

    nightmare

---
+++ index.js
@@ -0,0 +1,22 @@
+// index.js
+
+const { fetchMyIP, fetchISSFlyOverTimes, fetchCoordsByIP, nextISSTimesForMyLocation } = require('./iss');
+
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+// Step 0a - Calls function in iss.js expects error, passTimes back
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
